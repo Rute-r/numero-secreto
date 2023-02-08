24012e1346ebf98c70a2f203a0b20fbc99cca36f
Rute-r <rute_ke@hotmail.com>
2023-02-08 17:10:05 -0300

    pagina de jogo finalizado

---
+++ app/validacao.js
@@ -0,0 +1,57 @@
+function verificaSeOChutePossuiUmValorValido(chute) {
+  const numero = +chute;
+
+  if (chuteForInvalido(numero)) {
+    if (chute.toUpperCase() === 'FINALIZAR JOGO') {
+      document.body.innerHTML = `
+             <h2>Jogo finalizado</h2>
+             <h3>Pressione o botão para jogar novamente</h3>
+      
+             <button id='jogar-novamente' class='btn-jogar'>Jogar novamente <i class="fa-solid fa-arrow-rotate-right"></i></button>
+              
+              `;
+      document.body.style.backgroundColor = 'black';
+    } else {
+      elementoChute.innerHTML += '<div>Valor inválido</div>';
+    }
+    return;
+  }
+
+  if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+    elementoChute.innerHTML += `
+        <div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>
+        `;
+    return;
+  }
+
+  if (numero === numeroSecreto) {
+    document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+
+            <button id='jogar-novamente' class='btn-jogar'>Jogar novamente <i class="fa-solid fa-arrow-rotate-right"></i></button>
+        `;
+  } else if (numero > numeroSecreto) {
+    elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-angles-down"></i></div>
+        `;
+  } else {
+    elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-angles-up"></i></div>
+        `;
+  }
+}
+
+function chuteForInvalido(numero) {
+  return Number.isNaN(numero);
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+  return numero > maiorValor || numero < menorValor;
+}
+
+document.body.addEventListener('click', (e) => {
+  if (e.target.id == 'jogar-novamente') {
+    window.location.reload();
+  }
+});
